93c7bec501593c5a5b4212d7cbcd5779b1fa6e8b
Knikkey <102648803+Knikkey@users.noreply.github.com>
2023-07-07 15:00:42 +0900

    chore: idk what I did lol yolo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,32 @@
+"use client";
+
 import Image from "next/image";
 import googleIcon from "./_imgs/google-icon.png";
 import styles from "./page.module.scss";
 
 export default function Home() {
+  const handleLogin = async () => {
+    const res = await fetch("http://localhost:5000/auth/google");
+    const data = await res.json();
+    console.log(data);
+  };
+
   return (
     <main className={styles.main}>
       <div className={styles["content-container"]}>
         <h1>Goal Planner</h1>
         <h2>Divide and Conquor Your Goals</h2>
-        <button className={styles["login-btn"]}>
+        <a
+          href="http://localhost:5000/auth/google"
+          className={styles["login-btn"]}
+        >
           <Image
             src={googleIcon}
             alt="google icon"
             className={styles.icon}
           ></Image>
           Login With Google
-        </button>
+        </a>
       </div>
     </main>
   );
