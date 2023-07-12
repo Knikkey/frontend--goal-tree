7069bd03cf5ba8bd064312dc242e67124c833f00
Knikkey <102648803+Knikkey@users.noreply.github.com>
2023-07-12 15:58:05 +0900

    feature: implemented boilerplate for login button with mui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
-
 import Image from "next/image";
 import googleIcon from "./_imgs/google-icon.png";
+import { Button, Icon } from "@mui/material";
 import styles from "./page.module.scss";
 
 export default function Home() {
@@ -14,14 +14,29 @@ export default function Home() {
       <div className={styles["content-container"]}>
         <h1>Goal Planner</h1>
         <h2>Divide and Conquor Your Goals</h2>
-        <button onClick={handleLogin} className={styles["login-btn"]}>
+        {/* <button onClick={handleLogin} className={styles["login-btn"]}>
           <Image
             src={googleIcon}
             alt="google icon"
             className={styles.icon}
           ></Image>
           Login With Google
-        </button>
+        </button> */}
+        <Button
+          onClick={handleLogin}
+          variant="outlined"
+          startIcon={
+            <Icon className={styles.icon}>
+              <Image
+                src={googleIcon}
+                alt="google icon"
+                className={styles.icon}
+              />
+            </Icon>
+          }
+        >
+          Login With Google
+        </Button>
       </div>
     </main>
   );
