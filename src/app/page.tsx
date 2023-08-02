39137202fd3bf31e5793dfa8f326437ada901eeb
Knikkey <102648803+Knikkey@users.noreply.github.com>
2023-08-02 17:18:52 +0900

    style: converted to full mui style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,7 @@
 "use client";
 import Image from "next/image";
 import googleIcon from "./_imgs/google-icon.png";
-import { Button, Icon } from "@mui/material";
-import styles from "./page.module.scss";
+import { Button, Icon, Stack, Typography } from "@mui/material";
 
 export default function Home() {
   const handleLogin = async () => {
@@ -10,33 +9,47 @@ export default function Home() {
   };
 
   return (
-    <main>
-      <div className={styles["content-container"]}>
-        <h1>Goal Planner</h1>
-        <h2>Divide and Conquor Your Goals</h2>
-        <Button
-          onClick={handleLogin}
-          variant="outlined"
-          sx={{
-            color: "#111",
-            backgroundColor: "#fff",
-            "&:hover": {
-              backgroundColor: "#eee",
-            },
-          }}
-          startIcon={
-            <Icon className={styles.icon}>
-              <Image
-                src={googleIcon}
-                alt="google icon"
-                className={styles.icon}
-              />
-            </Icon>
-          }
-        >
-          Login With Google
-        </Button>
-      </div>
-    </main>
+    <Stack
+      component="main"
+      spacing={2}
+      justifyContent="center"
+      alignItems="center"
+    >
+      <Typography variant="h2" component="h1" lineHeight="0.75">
+        Goal Planner
+      </Typography>
+      <Typography variant="h5" component="h2">
+        Divide and Conquor Your Goals
+      </Typography>
+      <Button
+        onClick={handleLogin}
+        variant="outlined"
+        sx={{
+          color: "#111",
+          backgroundColor: "#fff",
+          marginTop: "1rem",
+          padding: "0.75rem 1.5rem",
+          "&:hover": {
+            backgroundColor: "#eee",
+          },
+        }}
+        startIcon={
+          <Icon sx={{ height: "auto", width: "100%", display: "flex" }}>
+            <Image
+              src={googleIcon}
+              alt="google icon"
+              style={{
+                maxWidth: "20px",
+                width: "100%",
+                height: "auto",
+                marginRight: "1rem",
+              }}
+            />
+          </Icon>
+        }
+      >
+        Login With Google
+      </Button>
+    </Stack>
   );
 }
